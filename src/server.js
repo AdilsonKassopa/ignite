7abd62490caa94fe7ecd5750a00d28b130b3c97b
AdilsonKassopa <adilsonkassopa@gmail.com>
2021-11-07 22:18:57 +0100

    criação da conta, verificar o estrato, verificar se a conta existe, fazer um deposito e fazer o saque

--- src/server.js
+++ src/server.js
@@ -1,21 +1,47 @@
+
 const { application } = require("express")
 const express = require("express")
 const {v4:uuid} = require("uuid")
 const App = express()
 App.use(express.json())
-const dbfase = []
+const customers = []
+
+function verifyExistAccountCPF(req,res,next){
+    const {cpf} = req.headers
+
+    const customer = customers.find((customer)=> customer.cpf === cpf)
+    if(!customer){
+        return res.status(400).json({Error:"customer not exist"})
+    }
+
+    req.customer=customer
+
+    return next()
+}
+
+function getBalance(statement){
+    const balance = statement.reduce((acc,operation)=>{
+        if(operation.type === "credit"){
+            return acc + operation.amount
+        }else{
+            return acc - operation.amount
+        }
+    },0)
+
+    return balance
+}
 
 
 App.post("/account",(req,res)=>{
     const {cpf,name} = req.body
     
-    const costumfAlreadExist = dbfase.some((dbfase)=> dbfase.cpf === cpf)
+    const costumfAlreadExist = customers.some((customers)=> customers.cpf === cpf)
 
     if(costumfAlreadExist){
         return res.status(400).json( {Error:"cpf alread exist"})
     }
 
-  dbfase.push({
+    customers.push({
       cpf,
       name,
       id:uuid(),
@@ -23,6 +49,49 @@ App.post("/account",(req,res)=>{
   })  
 
   return res.status(201).send()
+})
+
+App.get("/statement",verifyExistAccountCPF,(req,res)=>{
+   const {customer} = req
+
+    return res.json(customer.statement)
+    
+})
+
+App.post("/deposit",verifyExistAccountCPF,(req,res)=>{
+    const {description,amount} = req.body
+
+    const {customer } =req
+
+    const statementOperation = {
+        description,
+        amount,
+        created: new Date,
+        type:"credit"
+    }
+
+    customer.statement.push(statementOperation)
+    return res.status(201).send()
+})
+
+App.post("/withdraw",verifyExistAccountCPF,(req,res)=>{
+    const {amount} = req.body
+
+    const {customer} = req
+
+    const balance = getBalance(customer.statement)
+    if(balance < amount){
+        return res.status(400).json({Error:"insufienti funds"})
+    }
+
+    const statementOperation = {
+        amount,
+        created: new Date,
+        type:"debit"
+    }
+
+    customer.statement.push(statementOperation)
+    return res.status(201).send()
 })
 
 App.listen("3333",()=>{
